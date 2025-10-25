ef79536e405cf73bab01fb58d25d11f51ed65405
BhargavRaseshPatel <bhargavrpatelcanada@gmail.com>
2025-10-25 11:50:07 -0400

    backend set-up completed convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+
+  const todos = useQuery(api.todos.getTodos)
+  const addTodo = useMutation(api.todos.addTodo)
+  const clearAllTodos = useMutation(api.todos.clearAllTodos)
+  
   return (
     <View
       style={style.container}>
